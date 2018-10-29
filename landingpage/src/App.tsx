7ddd9dee70430824fb8c5ac3048ef36cdf1a287f
ivanoung <ivanoung@gmail.com>
2018-10-29 18:02:34 +0800

    added Semantic

--- landingpage/src/App.tsx
+++ landingpage/src/App.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import './App.css';
 
+import { Button } from 'semantic-ui-react';
+
+
+
 import logo from './logo.svg';
 
 class App extends React.Component {
@@ -10,6 +14,8 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <Button content='Primary' primary={true} />
+          <Button content='Secondary' secondary={true} disabled={true}/>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
