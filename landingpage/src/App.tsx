302d618921768ec28638323bd5f3c20ca67a5603
ivanoung <ivanoung@gmail.com>
2018-10-30 17:24:10 +0800

    moved landing page into another comp

--- landingpage/src/App.tsx
+++ landingpage/src/App.tsx
@@ -1,41 +1,20 @@
 // Importing modules
-import * as React from 'react';
-import './App.scss';
-
-import { Button } from "reactstrap";
-
-import ContentBox  from "./Components/ContentBox/ContentBox";
-import  InputBox from "./Components/InputBox/InputBox";
-import  SectionHeader  from "./Components/SectionHeader/SectionHeader";
-import Team from "./Components/Team/Team";
+import * as React from "react";
 
 // Importing styling and static assets
+import "./App.scss";
+
 // Importing reduc and friends
 // Importing react routering
 // Importing UI elements
-import HeroSection from "src/Components/HeroSection/HeroSection";
+import LandingPage from "./Pages/LandingPage/LandingPage";
 // Importing interfaces
 
 // import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
-    return (
-      <div className="App">
-        <header className="Hero">
-          <h1 className="Punchline">
-            Support Your Beloved <br /> Creative Media Projects
-          </h1>
-          <h3>Invest into what you believe in. Don't be an outsider.</h3>
-          <InputBox message="Subscribe for updates" />
-          <Button>Subscribe</Button>
-          <ContentBox/>
-          <SectionHeader header={"heading 1"} subHeader={"subHeading 1"}/>
-          <Team name={"Minsoo Kim"} title={"Founder and CEO"} picture={"./img/minsoo.jpg"}/>
-        </header>
-      <HeroSection/>
-      </div>
-    );
+    return <LandingPage />;
   }
 }
 
