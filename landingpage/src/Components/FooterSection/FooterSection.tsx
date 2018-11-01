51fb5b5d0622b7e3efca70c2f6171c8d30edf44c
Andrew Cheung <cheungdzinyung@gmail.com>
2018-11-01 18:21:22 +0800

    finished footer

--- landingpage/src/Components/FooterSection/FooterSection.tsx
+++ landingpage/src/Components/FooterSection/FooterSection.tsx
@@ -8,16 +8,18 @@ export default class FooterSection extends React.Component {
   public render() {
     return (
       <Row id="footerTop">
-        <Col>
-          <h3 className="Tagline">Share our vision</h3>
+        <Col xs="12" sm="12" md="12" lg="6">
+          <h3>Share our vision</h3>
           <p>
-            We are ambitious about our vision, but we need talented people like
-            you to grow.
+            We are ambitious about our vision, <br />
+            but we need talented people like you to grow.
           </p>
         </Col>
-        <Col className="footerSubscribe">
+        <Col id="footerJoin" xs="12" sm="12" md="12" lg="6">
           <Input message="Come join our team" />
-          <Button color="warning">Join our team</Button>
+          <Button color="warning" id="button">
+            Join our team
+          </Button>
         </Col>
       </Row>
     );
