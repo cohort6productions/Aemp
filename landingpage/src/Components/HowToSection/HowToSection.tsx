af40d1a6e39bcfdc2476682c0e8a6cd937c90bee
ivanoung <ivanoung@gmail.com>
2018-11-02 09:02:49 +0800

    carlouse file structure

---
+++ landingpage/src/Components/HowToSection/HowToSection.tsx
@@ -0,0 +1,13 @@
+// Importing modules
+import * as React from "react";
+// Importing styling and static assets
+// Importing reduc and friends
+// Importing react routering
+// Importing UI elements
+// Importing interfaces
+
+export default class InsertName extends React.Component {
+  public render() {
+    return <div>hi</div>;
+  }
+}
