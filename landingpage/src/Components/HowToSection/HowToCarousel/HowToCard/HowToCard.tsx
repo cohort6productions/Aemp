49294e7752dbda23a7fceb19e73f3ab5a6f43376
Andrew Cheung <cheungdzinyung@gmail.com>
2018-11-04 15:39:45 +0800

    woring on carousel shadow, commit to pull

--- landingpage/src/Components/HowToSection/HowToCarousel/HowToCard/HowToCard.tsx
+++ landingpage/src/Components/HowToSection/HowToCarousel/HowToCard/HowToCard.tsx
@@ -14,16 +14,16 @@ export default class HowToCard extends React.Component<IHowToCardProps> {
     return (
       <div className="howToCard">
         <Row>
-          <Col sm="12" md="6" className="howToContent">
+          <Col md="12" lg="6" className="howToContent">
             <h1>{this.props.heading}</h1>
             <h2>{this.props.subHeading}</h2>
             <p>{this.props.content}</p>
           </Col>
-          <Col sm="12" md="6" className="howToImage">
+          <Col md="12" lg="6" className="howToImage">
             <img
               src={this.props.picture}
               width="100%"
-              className="img-center center-block"
+              // className="img-center center-block"
             />
           </Col>
         </Row>
